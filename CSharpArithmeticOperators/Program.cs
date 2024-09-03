5c0de8930f42c3d67f587287c95b43683e987dd0
NatalieDillon <112569645+NatalieDillon@users.noreply.github.com>
2024-09-03 11:05:24 +0100

    Add Q1 and Q2

--- CSharpArithmeticOperators/Program.cs
+++ CSharpArithmeticOperators/Program.cs
@@ -1,24 +1,20 @@
 ﻿using System.Numerics;
 
-namespace CSharpArithmeticOperators
+namespace CSharpArithmeticOperatorExercises
 {
 	internal class Program
 	{
 		static void Main()
 		{
+			// Q1
+			double circumferenceCircle = ArithmeticOperators.CalcCircleCircumference(1.2);
+			Console.WriteLine($"The circumference of the circle is {Math.Round(circumferenceCircle, 2)}\n");
+
+			// Q2
 			double areaCircle = ArithmeticOperators.CalcCircleArea(1.2);
-			Console.WriteLine($"The area of the circle is {areaCircle}\n");
+			Console.WriteLine($"The area of the circle is {Math.Round(areaCircle,2)}\n");
 
-			ArithmeticOperators.Division();
-
-			decimal eatInCost = ArithmeticOperators.CalculateEatInCost(13);
-			Console.WriteLine($"The cost with VAT added is {eatInCost}\n");
-
-			ArithmeticOperators.Remainders();
-
-			ArithmeticOperators.Rounding();
-
-			ArithmeticOperators.DifferentRoundingOptions(areaCircle);
+			
 		}
 	}
 }
